8ae3baa5d807af34f9e58f68b46c62cae25c410e
georgevans <lgjt5248@leeds.ac.uk>
2024-10-17 16:00:17 +0000

    task_b.py and task_c.py

---
+++ task_b.py
@@ -0,0 +1,15 @@
+grade = input('Enter numerical grade between 0-100: ')
+if (grade.isnumeric()):
+    if grade >= 80 and grade <= 100:
+        print("Your grade is: A") 
+    elif grade <= 79 and grade >= 60:
+        print("Your grade is: B")
+    elif grade >= 50 and grade <= 59:
+        print("Your grade is: C")
+    elif grade >= 40 and grade <= 49:
+        print("Your grade is: D")
+    elif grade >= 0 and grade <= 39:
+        print("Your grade is: F")
+else:
+    print("Error: Please enter a number")
+    
---
+++ task_c.py
@@ -0,0 +1,7 @@
+pword = input('Enter a password that consists of letters and numbers only')
+if(len(pword) >= 8):
+    twotypes = False
+    if(pword.isdecimal() == False and pword.isalpha() == False):
+        print("Your password is valid")
+    else:
+        print("Your password must contain at least 8 characters, and a mix of letters and numbers")
