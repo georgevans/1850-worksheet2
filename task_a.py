4c4479e9cb66ceee8091a0751e5178e89f43e535
georgevans <lgjt5248@leeds.ac.uk>
2024-10-17 15:47:52 +0000

    task_a.py

---
+++ task_a.py
@@ -0,0 +1,18 @@
+weekdays = {
+    "monday": 1,
+    "tuesday": 2,
+    "wednesday": 3,
+    "thursday": 4,
+    "friday": 5,
+    "saturday": 6,
+    "sunday": 7
+}
+
+day = input('Enter day of the week: ').lower()
+
+if day in weekdays:
+    for n in weekdays.keys():
+        if day == n:
+            print(weekdays[n])
+else:
+    print('Please enter a valid day')
